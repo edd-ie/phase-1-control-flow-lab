8a98b21bc4e0c64debcb686ad0bb588b5012b789
edd-ie <ogendoeddie@gmail.com>
2023-03-13 14:08:28 +0300

    Complete

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-function scuberGreetingForFeet(){
+// TODO: free sample if the ride <= 400 feet
+// TODO: 30 dollars for a distance > 2000 feet
+// TODO: not allow rides > 2500 feet)
+
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  if (ride <= 400){
+    return ('This one is on me!');
+  }
+  else if (ride > 2000 && ride <= 2500){
+    return ('I will gladly take your thirty bucks.')
+  }
+  else if (ride > 2500){
+    return ('No can do.')
+  }
 }
 
-function ternaryCheckCity(){
+// TODO: returns "Ok, sounds good." city == NYC
+// TODO: should return "No go." != NYC
+
+function ternaryCheckCity(city){
   // Write your code here!
+  let check = city == 'NYC'
+  let result = ""
+  
+  check ? result = "Ok, sounds good." : result ="No go.";
+  return result;
 }
 
-function switchOnCharmFromTip(){
+// TODO: return "Thank you so much." tip == generous
+// TODO: return "Thank you." tip !generous
+// TODO: else return "Bye."
+
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip){
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye."
+  }
 }
